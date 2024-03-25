41133e448e5f1a033f3be80fa9bff52549c46b05
Suave101 <57422635+Suave101@users.noreply.github.com>
2024-03-25 00:09:16 -0500

    Note about Fathom Dash

--- frcdash.py
+++ frcdash.py
@@ -4,6 +4,9 @@ from flask import render_template
 app = Flask(__name__)
 # TODO: https://flask.palletsprojects.com/en/3.0.x/quickstart/#http-methods
 
+# To Run server:
+# python -m flask --app frcdash run
+
 
 @app.route("/")
 def hello_world():
